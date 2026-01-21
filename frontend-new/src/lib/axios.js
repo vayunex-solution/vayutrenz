cf20d074216a52b742ca9476a2b943d7647ee4dc
futurespheretechnologies <vayunexsolution@gmail.com>
2026-01-22 01:31:42 +0530

    fix: Update axios to use production API URL

--- frontend-new/src/lib/axios.js
+++ frontend-new/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BASE_URL = import.meta.env.MODE === 'development' 
   ? 'http://localhost:5001/api' 
-  : '/api'
+  : 'https://api.vayutrenz.vayunexsolution.com/api'
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
